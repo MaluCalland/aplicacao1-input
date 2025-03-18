1a964cd6fabdee975b26107ae8166e1fc47cb6be
MaluCalland <malucalland@gmail.com>
2025-03-18 10:10:15 -0300

    aula input

--- App.js
+++ App.js
@@ -1,4 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import { View, Text, StyleSheet, TextInput} from "react-native";
 
-class App extends Compo
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      nome: ''
+    };
+
+    this.entradaNome = this.entradaNome.bind(this);
+  };
+
+  entradaNome(texto){
+    if(texto.length > 0){
+      this.setState({nome: 'Obrigado por acessar: ' + texto})
+    } else {
+      this.setState({nome: ''})
+    }
+  }
+
+  render(){
+    return(
+      <View style={styles.container}>
+        <TextInput
+          style={styles.input}
+          placeholder='Digite seu nome: '
+          underlineColorAndroid='transparent'
+          onChangeText={this.entradaNome}
+        />
+
+        <Text style={styles.texto}>{this.state.nome}</Text>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1, //alinhamento no corpo
+  },
+  input:{
+    borderWidth: 1,
+    borderColor: 'blue',
+    margin: 20,
+    fontSize: 20,
+  },
+  texto:{
+    fontSize:18,
+    textAlign:'center',
+  }
+})
+
+export default App
